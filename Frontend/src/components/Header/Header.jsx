e483edd904f3758ce9c89fdbcfa8c7682fe6ca26
biswas lohani <lohanibiswas1@gmail.com>
2024-12-23 12:13:23 +0545

    removed

--- Frontend/src/components/Header/Header.jsx
+++ Frontend/src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
        <div className="header-contents">
         <h2>Order your favourite food here</h2>
         <p>Choose your favourite food from our diverse menu</p>
-        <button>View Menu</button>
+        
        </div>
     </div>
   )

7b2bd73ea5066dab6f88b75588cfba5188a3e72d
biswas lohani <lohanibiswas1@gmail.com>
2025-01-04 11:41:38 +0545

    Login

--- Frontend/src/components/Navbar/Navbar.jsx
+++ Frontend/src/components/Navbar/Navbar.jsx
@@ -1,39 +1,89 @@
-import React, { useContext, useState } from 'react'
-import './Navbar.css'
-import { assets } from '../../assets/assets'
-import { Link } from 'react-router-dom'
-import { StoreContext } from '../../context/StoreContext'
+import React, { useContext, useState } from "react";
+import "./Navbar.css";
+import { assets } from "../../assets/assets";
+import { Link, useNavigate } from "react-router-dom";
+import { StoreContext } from "../../context/StoreContext";
 
+const Navbar = ({ setShowlogin }) => {
+  // for Cart signal
+  const { getTotalCartAmount, token, setToken } = useContext(StoreContext);
 
-const Navbar = ({setShowlogin}) => {
+  const navigate = useNavigate();
 
-    // for Cart signal
-    const {getTotalCartAmount} = useContext(StoreContext)
+  const Logout = () => {
+    localStorage.removeItem("token");
+    setToken("");
+    navigate("/");
+  };
 
-    // creating a state variable
+  // creating a state variable
 
-    const [menu, setMenu] = useState("Home")
+  const [menu, setMenu] = useState("Home");
 
   return (
-    <div className='navbar'>
-        <Link to='/'><img src={assets.logo} alt="MealMate Logo" className='logo' /></Link> 
-        <ul className="navbar-menu">
-            <Link to='/' onClick={() => setMenu("Home")} className={menu === "Home"?"active":""}>Home</Link>
-            <a href='#explore-menu' onClick= {() => setMenu("Menu")}className={menu === "Menu"?"active":""}>Menu</a>
-            <a href='#Mobile-download' onClick={() => setMenu("Mobile-app")} className={menu === "Mobile-app"?"active":""}>Mobile-app</a>
-            <a href="#Footer" onClick={() => setMenu("Contact Us")}className={menu === "Contact Us"?"active":""}>Contact Us</a>
-        </ul>
-        <div className="navbar-right">
-            <img src={assets.search_icon} alt="" />
-            <div className="navbar-search-icon">
-                <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
-                <div className={getTotalCartAmount()===0?"":"dot"}></div>
-            </div>
-            <button onClick={() => setShowlogin(true)}>Sign Up/Login</button>
+    <div className="navbar">
+      <Link to="/">
+        <img src={assets.logo} alt="MealMate Logo" className="logo" />
+      </Link>
+      <ul className="navbar-menu">
+        <Link
+          to="/"
+          onClick={() => setMenu("Home")}
+          className={menu === "Home" ? "active" : ""}
+        >
+          Home
+        </Link>
+        <a
+          href="#explore-menu"
+          onClick={() => setMenu("Menu")}
+          className={menu === "Menu" ? "active" : ""}
+        >
+          Menu
+        </a>
+        <a
+          href="#Mobile-download"
+          onClick={() => setMenu("Mobile-app")}
+          className={menu === "Mobile-app" ? "active" : ""}
+        >
+          Mobile-app
+        </a>
+        <a
+          href="#Footer"
+          onClick={() => setMenu("Contact Us")}
+          className={menu === "Contact Us" ? "active" : ""}
+        >
+          Contact Us
+        </a>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="" />
+        <div className="navbar-search-icon">
+          <Link to="/cart">
+            <img src={assets.basket_icon} alt="" />
+          </Link>
+          <div className={getTotalCartAmount() === 0 ? "" : "dot"}></div>
         </div>
-      
+        {!token ? (
+          <button onClick={() => setShowlogin(true)}>Sign Up/Login</button>
+        ) : (
+          <div className="navbar-profile">
+            <img src={assets.profile_icon} alt="" />
+            <ul className="nav-profile-dropdown">
+              <li>
+                <img src={assets.bag_icon} alt="" />
+                <p>Orders</p>
+              </li>
+              <hr />
+              <li onClick={Logout}>
+                <img src={assets.logout_icon} alt="" />
+                <p>Logout</p>
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- Frontend/src/context/StoreContext.jsx
+++ Frontend/src/context/StoreContext.jsx
@@ -5,7 +5,8 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000"
+  const url = "http://localhost:4000";
+  const [token, setToken] = useState("");
   const addToCart = (itemId) => {
     if (!cartItems[itemId]) {
       setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
@@ -21,13 +22,18 @@ const StoreContextProvider = (props) => {
   const getTotalCartAmount = () => {
     let totalAmount = 0;
     for (const item in cartItems) {
-      if (cartItems[item]>0) {
+      if (cartItems[item] > 0) {
         let itemInfo = food_list.find((product) => product._id === item);
         totalAmount += itemInfo.price * cartItems[item];
       }
     }
     return totalAmount;
   };
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      setToken(localStorage.getItem("token"));
+    }
+  }, []);
 
   const contextValue = {
     food_list,
@@ -36,7 +42,9 @@ const StoreContextProvider = (props) => {
     addToCart,
     removeFromCart,
     getTotalCartAmount,
-    url
+    url,
+    token,
+    setToken,
   };
   return (
     <StoreContext.Provider value={contextValue}>

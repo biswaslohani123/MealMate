a85aa5bebdd356d5f2301ddeb2760c5375991bea
biswas lohani <lohanibiswas1@gmail.com>
2025-01-12 14:29:36 +0545

    made admin panel

--- Backend/routes/foodRoute.js
+++ Backend/routes/foodRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { addFood } from '../controllers/foodController.js'
+import { addFood, listfood, Removefood } from '../controllers/foodController.js'
 import multer from 'multer'
 
 const foodRouter = express.Router();
@@ -9,13 +9,15 @@ const foodRouter = express.Router();
 const storage = multer.diskStorage({
     destination: "uploads",
     filename:(req,file,cb) => {
-        return cb(null,`#{Date.now()}${file.originalname}`)
+        return cb(null,`${Date.now()}${file.originalname}`)
     }
 })
 
 const uploads = multer({storage:storage})
 
-foodRouter.post("/add",uploads.single("image"),addFood)
+foodRouter.post("/add",uploads.single("image"),addFood);
+foodRouter.get("/list",listfood);
+foodRouter.post("/remove",Removefood);
 
 
 

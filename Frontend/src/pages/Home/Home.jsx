3d4db7c526d1b6c429d32490fab6741928d5bb7d
biswas lohani <lohanibiswas1@gmail.com>
2025-02-28 10:18:54 +0545

    added features Our gallery and some changes in backend

--- Frontend/src/pages/Home/Home.jsx
+++ Frontend/src/pages/Home/Home.jsx
@@ -3,7 +3,8 @@ import './Home.css'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
-import DeliveryCharges from '../../components/Delivery Charges/OurGallery'
+import OurGallery from '../../components/Gallery/OurGallery'
+
 
 
 
@@ -16,7 +17,7 @@ const Home = () => {
       <Header/>
       <ExploreMenu category={category} setCategory={setCategory}/>
       <FoodDisplay category={category}/>
-     <DeliveryCharges/>
+      <OurGallery/>
      
       
       

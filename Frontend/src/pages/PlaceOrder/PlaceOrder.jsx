1d3a82b0552ad913277e213c7b60fb130cb0ad8a
biswas lohani <lohanibiswas1@gmail.com>
2025-03-14 13:23:27 +0545

    changes

--- Frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ Frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -91,7 +91,7 @@ const PlaceOrder = () => {
         <input name='email' onChange={onChangeHandler} value={data.email} type="email" placeholder='Email Address' required />
         <p>Your location:</p>
         <input name='location' onChange={onChangeHandler} value={data.location} type="text" placeholder='eg: Enter Your Address' required />
-        <input name='street' onChange={onChangeHandler} value={data.street} type="text" placeholder='eg: Enter Your Street.No' required />
+      
         
         
         <div className='multi-fields'>

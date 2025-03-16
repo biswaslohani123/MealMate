04a532b2fa87b208fa243fedba84b6c38dfce538
biswas lohani <lohanibiswas1@gmail.com>
2025-03-16 19:54:16 +0545

    some changes

--- Backend/routes/userRoute.js
+++ Backend/routes/userRoute.js
@@ -1,18 +1,14 @@
 import express from 'express'
-import { adminlogin, isAuthenticated, loginUser,logoutUser,registerUser, resetPassword, sendResetOTP, sendVerifyOtp, verifyEmail } from '../controllers/userController.js'
-import Auth from '../middleware/Auth.js'
+import { adminlogin, loginUser,registerUser } from '../controllers/userController.js'
+
 
 
 const userRouter = express.Router()
 
 userRouter.post("/register",registerUser)
 userRouter.post("/login",loginUser)
-userRouter.post("/logout", logoutUser )
-userRouter.post('/send-verify-otp', Auth, sendVerifyOtp)
-userRouter.post('/verify-account',Auth, verifyEmail)
-userRouter.post('/isAuth',Auth,isAuthenticated)
 userRouter.post('/admin',adminlogin)
-userRouter.post('/send-reset-otp', sendResetOTP)
-userRouter.post('/resetpassword', resetPassword)
+
+
 
 export default userRouter
--- Backend/server.js
+++ Backend/server.js
@@ -1,55 +1,35 @@
 import express from "express";
 import cors from "cors";
 import userRouter from "./routes/userRoute.js";
-import 'dotenv/config.js'
+import "dotenv/config.js";
 import { connectDB } from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
 import cartRouter from "./routes/cartRoute.js";
 import orderRouter from "./routes/orderRoute.js";
-import dotenv from 'dotenv';
-import usersRouter from './routes/authRoutes.js'
-
-
 
 // app config
-const app = express()
-const PORT = process.env.PORT || 4000
-
-
-
-
+const app = express();
+const PORT = process.env.PORT || 4000;
 
 // middleware
-app.use(express.json())
-app.use(cors({origin: allowedOrigins, credentials:true}))
-
-
+app.use(express.json());
+app.use(cors());
 
 // db connection
 connectDB();
-const allowedOrigins = ['http://localhost:5173']
-dotenv.config();
 
 // api endpoints
-app.use('/api/food',foodRouter)
-app.use("/images",express.static('uploads'))
-app.use("/api/user",userRouter)
-app.use("/api/auth", usersRouter)
-app.use("/api/cart",cartRouter)
-app.use("/api/order",orderRouter)
+app.use("/api/food", foodRouter);
+app.use("/images", express.static("uploads"));
+app.use("/api/user", userRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 
-
-
-
-
-app.get('/', (req, res) => {
-    res.send('API Working')
+app.get("/", (req, res) => {
+  res.send("API Working");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server started on http://localhost:${PORT}`);
-    
-})
+  console.log(`Server started on http://localhost:${PORT}`);
+});
 // mongodb+srv://Biswas:9814136254@cluster0.2vlb9.mongodb.net/?
-
-

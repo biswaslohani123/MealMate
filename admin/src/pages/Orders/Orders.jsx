e2a58f6900dcf04b61c131d9e52f6572b8458a52
biswas lohani <lohanibiswas1@gmail.com>
2025-04-17 21:26:39 +0545

    changes UI

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -200,7 +200,7 @@ const Orders = ({ url }) => {
                 placeholder="Search by name, email or items"
                 value={searchTerm}
                 onChange={(e) => setSearchTerm(e.target.value)}
-                className="w-full pl-10 pr-4 py-2.5 border border-amber-200 rounded-xl focus:ring-2 focus:ring-amber-400 focus:border-amber-400 transition-colors duration-200 bg-amber-50/50 text-gray-900"
+                className="w-full pl-10 pr-4 py-2.5 border border-amber-200 rounded-xl focus:ring-2 focus:ring-amber-400 focus:border-amber-400 transition-colors duration-200 text-gray-900"
               />
             </div>
 
@@ -264,7 +264,7 @@ const Orders = ({ url }) => {
                   <div className="flex flex-col lg:flex-row lg:items-center gap-4 md:gap-6">
                     <div className="flex-1">
                       <div className="flex items-start gap-3">
-                        <div className="p-2 bg-amber-50 rounded-lg border border-amber-100">
+                        <div className="p-2 rounded-lg border border-amber-100">
                           <Package className="h-5 w-5 text-orange-600" />
                         </div>
                         <div className="flex-1">
@@ -370,7 +370,7 @@ const Orders = ({ url }) => {
                 </div>
 
                 {expandedOrder === order._id && (
-                  <div className="p-5 border-t border-amber-100 animate-slideDown bg-amber-50/30">
+                  <div className="p-5 border-t border-amber-100 animate-slideDown ">
                     <div className="flex items-center justify-between mb-4">
                       <h4 className="font-medium text-gray-900">Order Details</h4>
                       

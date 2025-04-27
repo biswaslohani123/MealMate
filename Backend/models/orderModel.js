2b94ca1048491d9adcfcc02cce3adcc3e6fede30
biswas lohani <lohanibiswas1@gmail.com>
2025-04-27 16:54:33 +0545

    chnages

--- Backend/models/orderModel.js
+++ Backend/models/orderModel.js
@@ -1,15 +1,14 @@
 import mongoose from "mongoose";
 
 const orderSchema = new mongoose.Schema({
-    userId:{type:String,required:true},
-    items:{type:Array, required:true},
-    amount:{type:Number,required:true},
-    address:{type:Object,required:true},
-    status:{type:String,default:"Order Received"},
-    date:{type:Date, default:Date.now()},
-    payment:{type:Boolean,default:false},
-   
-}, {timestamps: true})
+    userId: {type: mongoose.Schema.Types.ObjectId,ref: "user",required: true},
+    items: { type: Array, required: true },
+    amount: { type: Number, required: true },
+    address: { type: Object, required: true },
+    status: { type: String, default: "Order Received" },
+    date: { type: Date, default: Date.now },
+    payment: { type: Boolean, default: false },
+}, { timestamps: true });
 
-const orderModel = mongoose.models.order || mongoose.model("order",orderSchema)
+const orderModel = mongoose.models.order || mongoose.model("order", orderSchema);
 export default orderModel;
